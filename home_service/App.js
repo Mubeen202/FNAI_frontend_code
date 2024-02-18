ec6b8286aa13a8efcbdb4053d07d67815cafd686
Mubeen202 <mubeensheikh202@gmail.com>
2024-02-18 06:36:14 -0800

    O Auth is completed

--- home_service/App.js
+++ home_service/App.js
@@ -2,14 +2,35 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Login from "./App/Screens/LoginScreen/Login";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+ 
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export default function App() {
   return (
-    <ClerkProvider publishableKey="pk_test_bm92ZWwtcmFtLTY2LmNsZXJrLmFjY291bnRzLmRldiQ">
+    <ClerkProvider
+    tokenCache={tokenCache}
+     publishableKey="pk_test_bm92ZWwtcmFtLTY2LmNsZXJrLmFjY291bnRzLmRldiQ"
+     >
       {/* <SafeAreaView style={styles.container}> */}
         {/* if you sign in then this is page  */}
-        <SignedIn>
-          <Text>You are Signed in</Text>
+        <SignedIn >
+          <Text style={{padding:50}}>You are Signed in</Text>
         </SignedIn>
         {/* if you are sign out redirect to this page */}
         <SignedOut>
--- home_service/App/Screens/LoginScreen/Login.jsx
+++ home_service/App/Screens/LoginScreen/Login.jsx
@@ -1,8 +1,32 @@
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import Colors from '../Utils/Colors'
-
+import * as WebBrowser from "expo-web-browser";
+import { useWarmUpBrowser } from "../Utils/hooks/useWarmUpBrowser";
+import { useOAuth } from "@clerk/clerk-expo";
+WebBrowser.maybeCompleteAuthSession();
 export default function Login() {
+  // Warm up the android browser to improve UX
+  // https://docs.expo.dev/guides/authentication/#improving-user-experience
+  useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+ 
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, []);
+ 
   return (
     <View>
       <View style={styles.container}>
@@ -18,7 +42,7 @@ export default function Login() {
 
           <Text style={styles.desc}>We deliver high-quality services to your doorstep.</Text> 
 
-          <TouchableOpacity style={styles.button} onPress={()=>{console.log('this is running')}}>
+          <TouchableOpacity style={styles.button} onPress={onPress}>
             <Text style={styles.buttonText}>Let's Started</Text>
           </TouchableOpacity>
         

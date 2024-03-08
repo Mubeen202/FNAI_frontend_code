699b11f71f54e8ad353f4472f180b756bf51277e
Mubeen202 <80257786+Mubeen202@users.noreply.github.com>
2024-03-08 12:22:45 +0400

    table is created

---
+++ home_service/App/Screens/TopNavigation/Premier/PremierTable/PremierTable.js
@@ -0,0 +1,135 @@
+export default PremierTableData = [
+    {
+      Pos: "1",
+      club: "AFC Bournemouth",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/AFC_Bournemouth.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+    {
+      Pos: "2",
+      club: "Arsenal FC",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/Arsenal FC.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+    {
+      Pos: "3",
+      club: "Aston Villa",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/Aston Villa.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+    {
+      Pos: "4",
+      club: "Brentford FC",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/Brentford FC.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+    {
+      Pos: "5",
+      club: "Brighton & Hove Albion",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/Brighton & Hove Albion.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+    {
+      Pos: "6",
+      club: "Burnley FC",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/Burnley FC.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+    {
+      Pos: "7",
+      club: "Chelsea FC",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/Chelsea FC.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+    {
+      Pos: "8",
+      club: "Crystal Palace",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/Crystal Palace.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+    {
+      Pos: "9",
+      club: "Everton FC",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/Everton FC.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+    {
+      Pos: "10",
+      club: "Fulham FC",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/Fulham FC.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+    {
+      Pos: "11",
+      club: "Liverpool FC",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/Liverpool FC.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+    {
+      Pos: "12",
+      club: "Luton Town",
+      icon: require("../../../../../assets/European Leagues/GB - Premier League/Luton Town.png"),
+      play: 10,
+      winning: 4,
+      losses:3,
+      draws:3,
+      pts: 5,
+    },
+  
+  ];
+  
--- home_service/App/Screens/TopNavigation/Premier/PremierTable/PremierTable.jsx
+++ home_service/App/Screens/TopNavigation/Premier/PremierTable/PremierTable.jsx
@@ -1,10 +1,170 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import Colors from '../../../Utils/Colors'; // Assuming you have a file defining colors
 
-export default function PremierTable() {
+const PremierTable = () => {
+
+    const [data, setData] = useState(PremierTableData);
+  useEffect(() => {
+    setData(PremierTableData);
+  }, []);
+
+
   return (
-    <View>
-      <Text>PremierTable</Text>
+    <View style={styles.container}>
+      {/* Table header */}
+      <View style={styles.row}>
+        {/* Left side columns */}
+        <View style={[styles.leftCell, styles.headerCell]}>
+          <Text style={styles.headerText}>POS</Text>
+        </View>
+        <View style={[styles.leftCell, styles.headerCell]}>
+          
+        </View>
+        <View style={[styles.leftCell, styles.headerCell]}>
+          <Text style={styles.headerText}>CLUB</Text>
+        </View>
+
+        {/* Right side columns */}
+        <View style={[styles.rightCell, styles.headerCell]}>
+          <Text style={styles.headerText}>P</Text>
+        </View>
+        <View style={[styles.rightCell, styles.headerCell]}>
+          <Text style={styles.headerText}>W</Text>
+        </View>
+        <View style={[styles.rightCell, styles.headerCell]}>
+          <Text style={styles.headerText}>D</Text>
+        </View>
+        <View style={[styles.rightCell, styles.headerCell]}>
+          <Text style={styles.headerText}>L</Text>
+        </View>
+        <View style={[styles.rightCell, styles.headerCell]}>
+          <Text style={styles.headerText}>Pts</Text>
+        </View>
+      </View>
+
+
+
+
+
+     {/* Table body */}
+         {/* Scrollable data section */}
+      <ScrollView style={styles.scrollableContainer}>
+        {data.map((item, index) => (
+          <View key={index} style={styles.row}>
+            <View style={styles.leftCell}>
+              <Text style={styles.cellText}>{item.Pos}</Text>
+            </View>
+            <View style={[styles.leftCell, styles.iconCell]}>
+              <Image source={item.icon} style={styles.icon} />
+            </View>
+            <View style={styles.leftCell}>
+              <Text numberOfLines={1} style={styles.cellTextName}>{item.club}</Text>
+            </View>
+            <View style={styles.rightCell}>
+              <Text style={styles.cellText}>{item.play}</Text>
+            </View>
+            <View style={styles.rightCell}>
+              <Text style={styles.cellText}>{item.winning}</Text>
+            </View>
+            <View style={styles.rightCell}>
+              <Text style={styles.cellText}>{item.draws}</Text>
+            </View>
+            <View style={styles.rightCell}>
+              <Text style={styles.cellText}>{item.losses}</Text>
+            </View>
+            <View style={styles.rightCell}>
+              <Text style={styles.cellText}>{item.pts}</Text>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
+{/* 
+      <View style={styles.row}>
+        <View style={styles.leftCell}>
+          <Text style={styles.cellText}>{item2.Pos}</Text>
+        </View>
+        <View style={[styles.leftCell, styles.iconCell]}>
+          <Image source={item2.icon} style={styles.icon} />
+          <Text numberOfLines={1} style={styles.cellTextName}>{item2.club}</Text>
+        </View>
+        <View style={styles.rightCell}>
+          <Text style={styles.cellText}>{item2.play}</Text>
+        </View>
+        <View style={styles.rightCell}>
+          <Text style={styles.cellText}>{item2.winning}</Text>
+        </View>
+        <View style={styles.rightCell}>
+          <Text style={styles.cellText}>{item2.draws}</Text>
+        </View>
+        <View style={styles.rightCell}>
+          <Text style={styles.cellText}>{item2.losses}</Text>
+        </View>
+        <View style={styles.rightCell}>
+          <Text style={styles.cellText}>{item2.pts}</Text>
+        </View>
+      </View> */}
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.LIGHT_GREY,
+    padding: 4,
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 2,
+  },
+  leftCell: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.DARK_GREY,
+    paddingVertical: 4,
+    paddingRight: 8,
+    
+  },
+  rightCell: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.DARK_GREY,
+    paddingVertical: 4,
+    paddingLeft: 8,
+  },
+  headerCell: {
+    backgroundColor: Colors.LIGHT_GREY,
+    textAlign: 'left',
+  },
+  headerText: {
+    color: Colors.WHITE,
+  },
+  cellText: {
+    color: Colors.WHITE,
+    fontSize: 12,
+  },
+  cellTextName: {
+    color: Colors.WHITE,
+    fontSize: 12,
+    width: 100,
+    paddingLeft: 6,
+    paddingRight: 6,
+  },
+  iconCell: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    marginRight: 35,
+  },
+});
+
+
+export default PremierTable;
